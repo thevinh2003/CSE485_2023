a8ef70f52387b5321de09cf69cd6a2e140e1eafa
thevinh2003 <91679717+thevinh2003@users.noreply.github.com>
2023-12-02 21:30:37 +0700

    Upload article features

--- index.php
+++ index.php
@@ -23,10 +23,13 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="./">Trang chủ</a>
+                        <a class="nav-link active" aria-current="page" href="./">Trang chủ</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="./login.php">Đăng nhập</a>
+                        <a class="nav-link" href="./login.php">Đăng nhập</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="admin/index.php">Admin</a>
                     </li>
                 </ul>
                 <form class="d-flex" role="search">
